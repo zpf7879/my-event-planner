1d872924daeda18d7f387498b2ec6d5d1cd26810
fpengzha <fpengzha@amazon.com>
2023-10-21 10:27:31 +0800

    Section 1.4: added Amplify; Force un-authenticated to home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
+// Adding Amplify Auth
+import { Auth } from "aws-amplify";
+
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: () => import("@/views/Home.vue"),
+    // Add meta line below
+    meta: { requiresAuth: true },
   },
   {
     path: "/signin",
@@ -18,5 +23,30 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 })
+
+// Adding method to chech if user is authenticated before moving navigating to the requested page
+router.beforeEach(async (to, from, next) => {
+  // Check if the route requires authentication by examining requiresAuth
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    try {
+      // Call the Amplify Auth method to check if the user is authenticated.
+      const user = await Auth.currentAuthenticatedUser();      
+      if (!user) {
+        // Not authenticated, so redirect to signin page.
+        next("/signin");
+      } 
+      else {
+        // They are authenticated, so we can continue.
+        next();
+      }
+    } catch (err) {
+      // Error, so redirect to signin page.
+      next("/signin");
+    }
+  } else {
+    // Route does not require authentication, so we can continue.
+    next();
+  }
+});
 
 export default router
